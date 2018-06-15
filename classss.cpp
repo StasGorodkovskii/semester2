807bd10bee986a0468a225b63aa3500053c5b5f0
StasGorodkovskii <31815930+StasGorodkovskii@users.noreply.github.com>
2018-06-15 15:31:34 +0300

    Update classss.cpp

--- classss.cpp
+++ classss.cpp
@@ -2,25 +2,38 @@
 #include"stdafx.h"
 
 using namespace std;
-class Students{ private:
+class Students{
+	private:
 
-	char* Name;
-	char* Surname;
-
+	char[20] Name;
+	int NumberOfPass;
+	       int BirthDate;
+                      int Finish;
 public:
+Students(char n,int num,int birth):Name(n),NumberOfPass(num),BirthDate(birth),Finish(finish)
+{
+finish=birth+21;
+	cout<<"The year of graduatings: "<<finish<<endl;
+	
+	
+}
 
+	       
 	void enterData()
 	{
 		cout << "Enter the Name: ";
 		cin >> Name;
-		cout << "Enter the Surname:";
-		cin >> Surname;
+		cout << "Enter the Age:";
+		cin >> Age;
+		cout << "Enter the NumberOfPasss:";
+		cin >> NumberOfPass;
 	}
 
 	void displayData()
 	{
 		cout << "Enter Name please: " <<Name << endl;
-		cout << "Enter Surname please" << Surname << endl;
+		cout << "Enter Age please" << Surname << endl;
+		cout << "Enter Nymber of passbook please" << Surname << endl;
 	}
 
 };
@@ -31,23 +44,23 @@ public:
 
 int main()
 {
-	Students StudentOne;
+	Students StudentOne('n',birth,num);
 	StudentOne.enterData();
 	StudentOne.displayData();
 	
-	Students StudentTwo;
+	Students StudentTwo('n',birth,num);
 	StudentTwo.enterData();
 	StudentTwo.displayData();
 	
-	Students StudentThree;
+	Students StudentThree('n',birth,num);
 	StudentThree.enterData();
 	StudentThree.displayData();
 	
-	Students StudentFour;
+	Students StudentFour('n',birth,num);
 	StudentFour.enterData();
 	StudentFour.displayData();
 	
-	Students StudentFive;
+	Students StudentFive('n',birth,num);
 	StudentFive.enterData();
 	StudentFive.displayData();
 	
